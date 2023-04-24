42fa3a575ac369196921a79d61612b8315397f10
Fernando Ribeiro de Amorim <nandoamori@yahoo.com.br>
2023-04-23 21:41:51 -0300

    criando models

---
+++ src/main/java/dio/nandoamori/labpadroesprojetospring/model/Cliente.java
@@ -0,0 +1,20 @@
+package dio.nandoamori.labpadroesprojetospring.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    @ManyToOne
+    private Endereco endereco;
+
+}
